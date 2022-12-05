072de253f3fdcaf487bada3f24da72378498b14d
David Refoua <David@Refoua.me>
2022-12-05 16:30:49 +0330

    check sapi environment

--- artisan-root.php
+++ artisan-root.php
@@ -1,3 +1,19 @@
 #!/usr/bin/env php
 <?php
 
+$GLOBALS['is_cli'] = php_sapi_name() === 'cli';
+
+try {
+	if (!$is_cli)
+		throw new Exception("This script must only be run in CLI.");
+}
+catch (Throwable $e) {
+	header("Content-Type: text/plain", true, 500);
+	if ($is_cli) {
+		$message = sprintf(chr(0x07) . "\e[91;1m%s\e[0m", $e->getMessage());
+	}
+	else {
+		$message = sprintf("%s", $e->getMessage());
+	}
+	die($message);
+}
