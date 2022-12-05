2eb9c9d018925443e9ab1f53bc3e33c05aa42062
David Refoua <David@Refoua.me>
2022-12-05 16:40:32 +0330

    find and execute artisan in any of the directories

--- artisan-root.php
+++ artisan-root.php
@@ -1,19 +1,43 @@
 #!/usr/bin/env php
 <?php
 
+$original_cwd = getcwd();
 $GLOBALS['is_cli'] = php_sapi_name() === 'cli';
 
 try {
 	if (!$is_cli)
 		throw new Exception("This script must only be run in CLI.");
+
+	$cwd = getcwd();
+
+	do {
+		if (empty($cwd) || empty($cwd == realpath($cwd)))
+			throw new Exception("Failed to detect the current working directory.");
+
+		if (file_exists('artisan'))
+			return require_once 'artisan';
+
+		if (!empty($parentDir = realpath(dirname($cwd))) && $parentDir != $cwd)
+			$cwd = $parentDir;
+		else
+			throw new Exception("Can not find `artisan` in the current directory structure.");
+
+		if (!chdir($cwd))
+			throw new Exception("Failed to lookup directory: $cwd");
+	}
+	while ($cwd);
 }
 catch (Throwable $e) {
-	header("Content-Type: text/plain", true, 500);
-	if ($is_cli) {
+	if (!headers_sent())
+		header("Content-Type: text/plain", true, 500);
+
+	if ($is_cli)
 		$message = sprintf(chr(0x07) . "\e[91;1m%s\e[0m", $e->getMessage());
-	}
-	else {
+	else
 		$message = sprintf("%s", $e->getMessage());
-	}
+
 	die($message);
+}
+finally {
+	chdir($original_cwd);
 }
